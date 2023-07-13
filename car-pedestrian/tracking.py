d9091f1487b02585093af981e2c3539b19515e73
George-Kibe <georgekibewambui@gmail.com>
2023-07-13 20:07:28 +0300

    detect images in video

--- car-pedestrian/tracking.py
+++ car-pedestrian/tracking.py
@@ -2,19 +2,46 @@ import cv2
 
 # Load Image
 img_file = "cars.jpg"
+# load video
+video = cv2.VideoCapture('Tesla.mp4')
 
 # Our Pre trained car classifier
 classifier_file = "car_detector.xml"
 # classifier_file = cv2.cv.Load("car_detection.xml")
 
+#create a car classifier
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
+# Run forever until car stops or something
+while True:
+    # read the current frame
+    (read_successful, frame) = video.read()
+    print(read_successful, frame)
+    
+    # Safe coding
+    if read_successful: 
+        # must convert to grayscale
+        grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        break
+    # Detect cars
+    cars = car_tracker.detectMultiScale(grayscaled_frame)
+    # draw rectangles in the car frames
+    for car in cars:
+        (x,y,w,h) = car
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+    #Display the image with the cars spotted
+    # cv2.imshow("Car Test Image", black_n_white_img)
+    cv2.imshow("Car Moving", frame)
+    # To avoid autoclose (wait and listen for the close key)
+    cv2.waitKey(1) 
+
+"""
 # create opencv image
 img = cv2.imread(img_file)
 
 # convert to grayscale (needed for haar cascade)
 black_n_white_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-#create a car classifier
-car_tracker = cv2.CascadeClassifier(classifier_file)
 
 # Detect cars
 cars = car_tracker.detectMultiScale(black_n_white_img)
@@ -30,10 +57,11 @@ for car in cars:
     (x,y,w,h) = car
     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
 
-#Display the image with the faces spotted
+#Display the image with the cars spotted
 # cv2.imshow("Car Test Image", black_n_white_img)
 cv2.imshow("Car Test Image", img)
 # To avoid autoclose (wait and listen for the close key)
-cv2.waitKey() 
+cv2.waitKey(10) 
 
+"""
 print("Code processing completed!")

004f1700f9c8c2c4f7e9fda060200315d15efd67
George-Kibe <georgekibewambui@gmail.com>
2023-08-14 17:52:42 +0300

    classification examples

--- ImageManipulation/mobile-app/resize.py
+++ ImageManipulation/mobile-app/resize.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
 # Open the original image
-for i in range(10):
+for i in range(1,10):
     print(i)    
-    with Image.open('1.png') as img:
+    with Image.open(f'{i}.png') as img:
         # Resize the image to 1200px by 148px using the ANTIALIAS filter
         resized_img = img.resize((1000, 2200), resample=Image.LANCZOS)    
         # Save the resized image
